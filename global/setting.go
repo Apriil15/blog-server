183a64591c842a76ca6919c32603c3794b674faa
nlwkobe30 <nlwkobe30@gmail.com>
2021-05-06 00:52:08 +0800

    add setting

---
+++ global/setting.go
@@ -0,0 +1,9 @@
+package global
+
+import "github.com/Apriil15/blog-server/pkg/setting"
+
+var (
+	ServerSetting   *setting.ServerSettingS
+	AppSetting      *setting.AppSettingS
+	DatabaseSetting *setting.DatabaseSettingS
+)
