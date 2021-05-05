20626596806eeeb343dcc0fc3799a0bd33099aa5
nlwkobe30 <nlwkobe30@gmail.com>
2021-05-06 01:59:13 +0800

    replace configs in main.go

--- main.go
+++ main.go
@@ -1,21 +1,59 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"time"
 
+	"github.com/Apriil15/blog-server/global"
 	"github.com/Apriil15/blog-server/internal/routers"
+	"github.com/Apriil15/blog-server/pkg/setting"
+	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	err := setupSetting()
+	if err != nil {
+		log.Fatalf("init setupSetting err: %v", err)
+	}
+}
+
 func main() {
+	gin.SetMode(global.ServerSetting.RunMode)
 	router := routers.NewRouter()
 
 	s := &http.Server{
-		Addr:           ":8080",
+		Addr:           ":" + global.ServerSetting.HttpPort,
 		Handler:        router,
-		ReadTimeout:    time.Second * 10,
-		WriteTimeout:   time.Second * 10,
+		ReadTimeout:    global.ServerSetting.ReadTimeout,
+		WriteTimeout:   global.ServerSetting.WriteTimeout,
 		MaxHeaderBytes: 1 << 20,
 	}
 	s.ListenAndServe()
+}
+
+func setupSetting() error {
+	setting, err := setting.NewSetting()
+	if err != nil {
+		return err
+	}
+
+	err = setting.ReadSection("Server", &global.ServerSetting)
+	if err != nil {
+		return err
+	}
+
+	err = setting.ReadSection("App", &global.AppSetting)
+	if err != nil {
+		return err
+	}
+
+	err = setting.ReadSection("Database", &global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+
+	global.ServerSetting.ReadTimeout *= time.Second
+	global.ServerSetting.WriteTimeout *= time.Second
+	return nil
 }
