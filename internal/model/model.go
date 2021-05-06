d4d0919777f73729454b4f4183db258bb57393ae
nlwkobe30 <nlwkobe30@gmail.com>
2021-05-06 21:56:56 +0800

    update NewDBEngine function

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,5 +1,16 @@
 package model
 
+import (
+	"fmt"
+
+	"github.com/Apriil15/blog-server/global"
+	"github.com/Apriil15/blog-server/pkg/setting"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
+)
+
 type Model struct {
 	ID         uint32 `gorm:"primary_key" json:"id"`
 	CreatedOn  uint32 `json:"created_On"`
@@ -8,4 +19,35 @@ type Model struct {
 	ModifiedBy string `json:"modified_by"`
 	DeletedOn  uint32 `json:"deleted_on"`
 	IsDel      uint8  `json:"is_del"`
+}
+
+func NewDBEngine(databaseSetting *setting.DatabaseSettingS) (*gorm.DB, error) {
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=%s&parseTime=%t&loc=Local",
+		databaseSetting.UserName,
+		databaseSetting.Password,
+		databaseSetting.Host,
+		databaseSetting.DBName,
+		databaseSetting.Charset,
+		databaseSetting.ParseTime,
+	)
+
+	gormDB, err := gorm.Open(mysql.Open(connectionString), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	sqlDB, err := gormDB.DB()
+	if err != nil {
+		return nil, err
+	}
+
+	sqlDB.SetMaxIdleConns(global.DatabaseSetting.MaxIdleConns)
+	sqlDB.SetMaxOpenConns(global.DatabaseSetting.MaxOpenConns)
+
+	return gormDB, nil
 }
