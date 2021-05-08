b34dd9c50d10a02c90db76f1fbd08c91d7d725d8
nlwkobe30 <nlwkobe30@gmail.com>
2021-05-09 04:44:56 +0800

    update tag.go for service layer

--- internal/service/tag.go
+++ internal/service/tag.go
@@ -1,5 +1,10 @@
 package service
 
+import (
+	"github.com/Apriil15/blog-server/internal/model"
+	"github.com/Apriil15/blog-server/pkg/app"
+)
+
 type CountTagRequest struct {
 	Name  string `form:"name" binding:"max=100"`
 	State uint8  `form:"state,default=1" binding:"oneof=0 1"`
@@ -25,4 +30,24 @@ type UpdateTagRequest struct {
 
 type DeleteTagRequest struct {
 	ID uint32 `form:"id" binding:"required,gte=1"`
+}
+
+func (s *Service) CountTag(param *CountTagRequest) (int64, error) {
+	return s.dao.CountTag(param.Name, param.State)
+}
+
+func (s *Service) GetTagList(param *TagListRequest, pager *app.Pager) ([]*model.Tag, error) {
+	return s.dao.GetTagList(param.Name, param.State, pager.Page, pager.PageSize)
+}
+
+func (s *Service) CreateTag(param *CreateTagRequest) error {
+	return s.dao.CreateTag(param.Name, param.State, param.CreatedBy)
+}
+
+func (s *Service) UpdateTag(param *UpdateTagRequest) error {
+	return s.dao.UpdateTag(param.ID, param.Name, param.State, param.ModifiedBy)
+}
+
+func (s *Service) DeleteTag(param *DeleteTagRequest) error {
+	return s.dao.DeleteTag(param.ID)
 }
