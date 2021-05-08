a7a7e7b426a4c27cbe5816996b6d0c79e9bebba7
nlwkobe30 <nlwkobe30@gmail.com>
2021-05-08 18:12:14 +0800

    fix: need a space between numbers, or ithere would be a problem

--- internal/service/Article.go
+++ internal/service/Article.go
@@ -2,12 +2,12 @@ package service
 
 type CountArticleRequest struct {
 	Name  string `form:"name" binding:"max=100"`
-	State uint8  `form:"state,default=1" binding:"oneof=01"`
+	State uint8  `form:"state,default=1" binding:"oneof=0 1"`
 }
 
 type ArticleListRequest struct {
 	Title string `form:"title" binding:"max=100"`
-	State uint8  `form:"state,default=1" binding:"oneof=01"`
+	State uint8  `form:"state,default=1" binding:"oneof=0 1"`
 }
 
 type CreateArticleRequest struct {
@@ -16,14 +16,14 @@ type CreateArticleRequest struct {
 	Content       string `form:"content" binding:"required,min=1,max=1000"`
 	CoverImageUrl string `form:"cover_image_url" binding:"min=1,max=100"`
 	CreatedBy     string `form:"created_by" binding:"required,min=3,max=100"`
-	State         uint8  `form:"state,default=1" binding:"oneof=01"`
+	State         uint8  `form:"state,default=1" binding:"oneof=0 1"`
 }
 
 type UpdateArticleRequest struct {
 	ID            uint32 `form:"id" binding:"required,gte=1"`
 	Title         string `form:"title" binding:"min=1,max=100"`
 	Desc          string `form:"desc" binding:"min=1,max=100"`
-	State         uint8  `form:"state" binding:"required,oneof=01"`
+	State         uint8  `form:"state" binding:"required,oneof=0 1"`
 	ModifiedBy    string `form:"modified_by" binding:"required,min=3,max=100"`
 	Content       string `form:"content" binding:"min=1,max=1000"`
 	CoverImageUrl string `form:"cover_image_url" binding:"min=1,max=100"`
