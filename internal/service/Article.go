f086fe811cc296db52e93b915198c7f8da3c067b
nlwkobe30 <nlwkobe30@gmail.com>
2021-05-11 01:06:18 +0800

    fix: can't create article when there is no "cover_image_url" param
    modify validator of binding min to 0

--- internal/service/Article.go
+++ internal/service/Article.go
@@ -14,7 +14,7 @@ type CreateArticleRequest struct {
 	Title         string `form:"title" binding:"required,min=1,max=100"`
 	Desc          string `form:"desc" binding:"required,min=1,max=100"`
 	Content       string `form:"content" binding:"required,min=1,max=1000"`
-	CoverImageUrl string `form:"cover_image_url" binding:"min=1,max=100"`
+	CoverImageUrl string `form:"cover_image_url" binding:"min=0,max=100"`
 	CreatedBy     string `form:"created_by" binding:"required,min=3,max=100"`
 	State         uint8  `form:"state,default=1" binding:"oneof=0 1"`
 }
