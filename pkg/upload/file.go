f86d66d7553b5a515057ee7b91eddc2490068ad6
nlwkobe30 <nlwkobe30@gmail.com>
2021-05-14 00:34:01 +0800

    feat: update file.go

--- pkg/upload/file.go
+++ pkg/upload/file.go
@@ -1,6 +1,10 @@
 package upload
 
 import (
+	"io"
+	"io/ioutil"
+	"mime/multipart"
+	"os"
 	"path"
 	"strings"
 
@@ -32,4 +36,73 @@ func GetFileExtension(name string) string {
 // Get upload save path
 func GetUploadSavePath() string {
 	return global.AppSetting.UploadSavePath
+}
+
+// Check whether save path is exist
+func CheckSavePath(destination string) bool {
+	_, err := os.Stat(destination) // get FileInfo
+	return os.IsNotExist(err)
+}
+
+// Check extension is supported
+func CheckContainExtension(fileType FileType, name string) bool {
+	extension := GetFileExtension(name)
+	extension = strings.ToUpper(extension) // maybe extension with upper, lower, or mix, so i use ToUpper to uniform
+
+	switch fileType {
+	case TypeImage:
+		for _, allowExtension := range global.AppSetting.UploadImageAllowExtensions {
+			if extension == strings.ToUpper(allowExtension) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+// Check image size
+func CheckMaxSize(fileType FileType, file multipart.File) bool {
+	content, _ := ioutil.ReadAll(file)
+	size := len(content)
+
+	switch fileType {
+	case TypeImage:
+		if size >= global.AppSetting.UploadImageMaxSize*1024*1024 {
+			return true
+		}
+	}
+	return false
+}
+
+// Check permission
+func CheckPermission(destination string) bool {
+	_, err := os.Stat(destination)
+	return os.IsPermission(err)
+}
+
+// Create save path
+func CreateSavePath(destination string, permission os.FileMode) error {
+	err := os.MkdirAll(destination, permission)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Save file to destinationPath
+func SaveFile(file *multipart.FileHeader, destination string) error {
+	source, err := file.Open()
+	if err != nil {
+		return err
+	}
+	defer source.Close()
+
+	destinationFile, err := os.Create(destination)
+	if err != nil {
+		return err
+	}
+	defer destinationFile.Close()
+
+	_, err = io.Copy(destinationFile, source) // copy from source to destinationFile
+	return err
 }
